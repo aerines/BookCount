ce4857711a71216632fed72a91c04bfb2169928f
Rahel <rahel.eslas@gmail.com>
2022-01-22 10:49:08 +0200

    Initial commit

---
+++ Weekday.java
@@ -0,0 +1,22 @@
+class countTheBooks {
+    public static void main(String[] args) {
+        int bookCount = 78;
+        int answerImput = 0;
+
+        if (answerImput = 78) {
+            System.out.println("Yay! You are correct, I have 94 books!");
+        } else if (answerImput >= 100) {
+            System.out.println("In this economy? That's way too much! Try numbers under 100.");
+        } else if (answerImput > 78 && answerImput < 100) {
+            System.out.println("I wish! Try a little less.");
+        } else if (answerImput < 78 && answerImput > 0) {
+            System.out.println("I've had some good paydays in my life. Try a little more.");
+        } else if (answeImput = 0) {
+            System.out.println("I wish I didn't know how to read, but here we are.")
+        } else if (answerImput < 0) {
+            System.out.println("What is this, 'Fahrenheit 451'?");
+        }
+
+
+    }
+}
